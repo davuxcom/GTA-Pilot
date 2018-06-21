2267d69d0f6bd5f311899c94d64569553bba34a3
Amenta <dave@daveamenta.com>
2018-06-21 04:50:33 -0700

    More flight plan skeleton

--- src/app/FlightPlanBuidler.xaml.cs
+++ src/app/FlightPlanBuidler.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
@@ -9,21 +11,45 @@ namespace GTAPilot
 {
     public partial class FlightPlanBuidler : Window
     {
+        public class Position
+        {
+            public Point pt;
+            public double Heading;
+
+            public override string ToString()
+            {
+                return $"{(int)pt.X}, {(int)pt.Y} Heading={Math.Round(Heading,2)}";
+            }
+        }
+
+        public ObservableCollection<Position> Positions { get; }
+
         public FlightPlanBuidler()
         {
             InitializeComponent();
 
             img.Source = new BitmapImage(new Uri(System.IO.Path.GetFullPath("../../../../res/map_zoom4_full_20.png")));
 
+            Positions = new ObservableCollection<Position>();
+
+            var start = new Point(2030.2f, 4573.9f);
+
+            AddPosition(new Point(start.X / 5, start.Y / 5));
+            AddPosition(new Point(426, 880));
+
+            DataContext = this;
         }
 
         Point lastPoint;
 
         private void img_MouseRightButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            var pt = e.GetPosition(img);
-            Trace.WriteLine(e.GetPosition(img));
+            AddPosition(e.GetPosition(img));
+        }
 
+        private void AddPosition(Point pt)
+        {
+            double heading = double.NaN;
             if (lastPoint != default(Point))
             {
                 var l = new Line
@@ -34,11 +60,23 @@ namespace GTAPilot
                     Y2 = lastPoint.Y
                 };
                 l.Stroke = Brushes.Blue;
-                l.StrokeThickness = 2;
+                l.StrokeThickness = 1;
                 canvas.Children.Add(l);
+
+                heading = Math.Atan2(l.Y2 - l.Y1, l.X2 - l.X1);
+
+              //  heading = 90 - (heading * (180/ Math.PI)) + (heading > 0 ? 0 : 360);
             }
 
+            Ellipse dot = new Ellipse();
+            dot.Fill = Brushes.Red;
+            dot.Height = dot.Width = 2;
+            Canvas.SetTop(dot, pt.Y - dot.Height / 2);
+            Canvas.SetLeft(dot, pt.X - dot.Width / 2);
+            canvas.Children.Add(dot);
             lastPoint = pt;
+
+            Positions.Add(new Position { pt = pt, Heading = heading });
         }
     }
 }
