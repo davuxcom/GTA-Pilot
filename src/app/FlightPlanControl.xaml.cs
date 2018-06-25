8c1c4bb971d52177ae2cff8ba87dce6b9eb9e7bb
Amenta <dave@daveamenta.com>
2018-06-24 23:45:50 -0700

    Skeleton flight planner in dashboard

---
+++ src/app/FlightPlanControl.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+
+namespace GTAPilot
+{
+    public partial class FlightPlanControl : UserControl
+    {
+        public RelayCommand Open { get; }
+        public RelayCommand Save { get; }
+        public RelayCommand Edit { get; }
+
+     //   public ObservableCollection<Points>
+
+        public FlightPlanControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
